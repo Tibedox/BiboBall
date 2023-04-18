b738e9e0d6c80072ebb4b335353e2945d8be4eb1
Tibedox <tibedox@mail.ru>
2023-04-18 11:17:55 +0600

    ChainShape62

--- core/src/ru/tibedox/biboball/BiBoBall.java
+++ core/src/ru/tibedox/biboball/BiBoBall.java
@@ -26,6 +26,7 @@ public class BiBoBall extends ApplicationAdapter {
     StaticBox[] wall = new StaticBox[5];
 	DynamicCircle ball;
     DynamicBox brick;
+    DynamicPoly starball;
     KinematicCircle biba, boba;
     KinematicPoly star;
 
@@ -53,14 +54,15 @@ public class BiBoBall extends ApplicationAdapter {
         boba = new KinematicCircle(world, WORLD_WIDTH/4*3, 0.7f, 0.7f);
         ball = new DynamicCircle(world, WORLD_WIDTH/2, WORLD_HEIGHT-1, 0.7f);
         ball.body.applyForceToCenter(100.0f*rnd(), 0f, true);
+        starball = new DynamicPoly(world, WORLD_WIDTH/2-1.1f, WORLD_HEIGHT-1.1f);
         star = new KinematicPoly(world, WORLD_WIDTH/4, WORLD_HEIGHT/2, -4);
         new KinematicPoly(world, WORLD_WIDTH/4*3, WORLD_HEIGHT/2, 4);
 
-        new KinematicPoly(world, WORLD_WIDTH/8, WORLD_HEIGHT/6, -2);
-        new KinematicPoly(world, WORLD_WIDTH/8*7, WORLD_HEIGHT/6, 2);
+        new KinematicPoly(world, WORLD_WIDTH/8, WORLD_HEIGHT/6, -5);
+        new KinematicPoly(world, WORLD_WIDTH/8*7, WORLD_HEIGHT/6, 5);
 
-        new KinematicPoly(world, WORLD_WIDTH/8*3, WORLD_HEIGHT/6, 3);
-        new KinematicPoly(world, WORLD_WIDTH/8*5, WORLD_HEIGHT/6, -3);
+        new KinematicPoly(world, WORLD_WIDTH/8*3, WORLD_HEIGHT/6, -5);
+        new KinematicPoly(world, WORLD_WIDTH/8*5, WORLD_HEIGHT/6, 5);
         //brick = new Brick(world, WORLD_WIDTH/2, WORLD_HEIGHT-10, 8, 4);
         //brick.body.applyForceToCenter(10000.0f*rnd(), 0f, true);
     }
--- core/src/ru/tibedox/biboball/DynamicPoly.java
+++ core/src/ru/tibedox/biboball/DynamicPoly.java
@@ -1,42 +1,67 @@
 package ru.tibedox.biboball;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.ChainShape;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Json;
+
+import java.util.ArrayList;
+
+import jdk.nashorn.internal.parser.JSONParser;
 
 public class DynamicPoly {
     Body body;
     float x, y;
     float width, height;
     float angle;
-    float r;
 
-    public DynamicPoly(World world, float x, float y, float r) {
+    Vector2[] chain;
+
+    public DynamicPoly(World world, float x, float y) {
         BodyDef bodyDef = new BodyDef(); // создаём объект настроек тела
         bodyDef.type = BodyDef.BodyType.DynamicBody; // определяем тип
         bodyDef.position.set(x, y); // устанавливаем стартовую позицию
 
         body = world.createBody(bodyDef); // создаём само тело
 
-        CircleShape circle = new CircleShape(); // создаём объект формы
-        circle.setRadius(r);
+        fromJson();
+        ChainShape chainShape = new ChainShape(); // создаём объект формы
+        chainShape.createChain(chain);
 
         FixtureDef fixtureDef = new FixtureDef(); // Создаём настройки fixture для тела
-        fixtureDef.shape = circle; // форма
-        fixtureDef.density = 0.5f; // плотность
+        fixtureDef.shape = chainShape; // форма
+        fixtureDef.density = 0.3f; // плотность
         fixtureDef.friction = 0.4f; // трение
-        fixtureDef.restitution = 0.8f; // упругость /Make it bounce a little bit
+        fixtureDef.restitution = 0.9f; // упругость /Make it bounce a little bit
 
         Fixture fixture = body.createFixture(fixtureDef); // создаём fixture
 
-        circle.dispose(); // форму необходимо удалять
+        chainShape.dispose(); // форму необходимо удалять
+        body.setAngularVelocity(1);
+    }
 
-        this.r = r;
-        width = height = r*2;
+    void fromJson() {
+        // read from json
+        FileHandle file = Gdx.files.internal("star.json");
+        String jsonString = file.readString();
+        Json json = new Json();
+        Vertices verts;
+        verts = json.fromJson(Vertices.class, jsonString);
+
+        System.out.println(verts.toString());
+        chain = new Vector2[verts.vertices.length];
+        for (int i = 0; i < chain.length; i++) {
+            chain[i] = new Vector2(verts.vertices[i].x/100-1, verts.vertices[i].y/100-1);
+            System.out.print(chain[i].x+" "+ chain[i].y+" - ");
+        }
     }
 
     void update(){
@@ -64,4 +89,10 @@ public class DynamicPoly {
     public float getAngle() {
         return MathUtils.radiansToDegrees*body.getAngle();
     }
+}
+class Vertice{
+    float x, y;
+}
+class Vertices{
+    Vertice[] vertices;
 }
